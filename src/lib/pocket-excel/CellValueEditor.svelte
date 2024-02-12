1b61cd0ac77ef438571a0045f5cb8cf6ab5e16c5
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-12 17:57:17 +0900

    セルエディタのスタイリング

--- src/lib/pocket-excel/CellValueEditor.svelte
+++ src/lib/pocket-excel/CellValueEditor.svelte
@@ -6,6 +6,13 @@
 
 <style>
   textarea {
+    padding: 0.25em 0.56em;
     resize: none;
+    border-color: var(--excel__cell__border-color);
+    border-radius: 5px;
+    box-shadow: 0px 3px 5px rgba(0, 0, 0, 0.04);
+    font-family: var(--excel__font);
+    font-weight: var(--excel__font-weight);
+    font-size: var(--excel__font-size);
   }
 </style>
