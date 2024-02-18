55f0f5b50fc428ffb8905a518d123d79b4111af3
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-19 07:31:33 +0900

    rehype-pretty-code導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import mdx from "@astrojs/mdx"
+import rehypePrettyCode from "rehype-pretty-code"
+
+const prettyCodeOptions = {
+  theme: "material-theme-lighter",
+  keepBackground: false,
+  defaultLang: "elm"
+}
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), mdx()],
   markdown: {
-    shikiConfig: {
-      theme: "slack-ochin"
-    }
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]]
   }
 })
