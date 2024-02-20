4b1a5b12552da697796a8dbd20c211e3bd505027
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-20 18:34:37 +0900

    色情報を持たせたり

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,21 @@
 import { z, defineCollection, reference } from "astro:content"
 
 const zCategory = z.enum(["lookup", "string", "condition"]).transform((val) => {
-  if (val === "lookup") return "表引き"
-  if (val === "string") return "文字列操作"
-  if (val === "condition") return "条件分岐"
+  if (val === "lookup")
+    return {
+      label: "表引き",
+      color: "#fdba74"
+    }
+  if (val === "string")
+    return {
+      label: "文字列操作",
+      color: "#bef264"
+    }
+  if (val === "condition")
+    return {
+      label: "条件分岐",
+      color: "#c4b5fd"
+    }
 })
 
 const zAvailableVersion = z
@@ -16,33 +28,57 @@ const zAvailableVersion = z
   )
   .default(["old"])
 
+const zFnArgument = z.object({
+  summary: z.string(),
+  default: z.string().optional(),
+  pattern: z
+    .array(
+      z.object({
+        value: z.string(),
+        behavior: z.string()
+      })
+    )
+    .optional()
+})
+
+export type FnArgument = z.infer<typeof zFnArgument>
+
+const zValueType = z.enum(["string", "number", "boolean", "error"]).transform((val) => {
+  if (val === "string")
+    return {
+      label: "文字列",
+      color: "#bef264"
+    }
+  if (val === "number")
+    return {
+      label: "数値",
+      color: "#67e8f9"
+    }
+  if (val === "boolean")
+    return {
+      label: "真偽値",
+      color: "#c4b5fd"
+    }
+  if (val === "error")
+    return {
+      label: "エラー",
+      color: "#fca5a5"
+    }
+})
+
 const zFnReturn = z.object({
-  type: z.enum(["string", "number", "boolean", "error"]),
+  type: zValueType,
   summary: z.coerce.string(),
   if: z.string().optional()
 })
+export type FnReturn = z.infer<typeof zFnReturn>
 
 const functionCollections = defineCollection({
   type: "content",
   schema: z.object({
     name: z.string(),
     summary: z.string(),
-    args: z
-      .array(
-        z.object({
-          summary: z.string(),
-          default: z.string().optional(),
-          pattern: z
-            .array(
-              z.object({
-                value: z.string(),
-                behavior: z.string()
-              })
-            )
-            .optional()
-        })
-      )
-      .default([]),
+    args: z.array(zFnArgument).default([]),
     return: z.union([zFnReturn, z.array(zFnReturn.required())]).optional(),
     category: zCategory,
     available: zAvailableVersion
