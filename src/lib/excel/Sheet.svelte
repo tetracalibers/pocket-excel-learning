78f4def5f8590bee613178f4a2f1039d9f4c0146
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-21 17:36:46 +0900

    lib/excel: マウス操作でセル範囲を選択できるところまで実装

---
+++ src/lib/excel/Sheet.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import SelectOverlay from "./components/overlay/SelectOverlay.svelte"
+  import ExtendOverlay from "./components/overlay/ExtendOverlay.svelte"
+  import DraggableSquare from "./components/overlay/DraggableSquare.svelte"
+  // import CellValueEditor from "./components/cell/CellValueEditor.svelte"
+  import ExTable from "./ExTable.svelte"
+  import { useSpreadSheet } from "./hooks/use-sheet"
+  import { getHeaders, toTableArray } from "./utils/data"
+
+  export let data: Array<Record<string, unknown>> = []
+
+  let headers = getHeaders(data)
+  let table = toTableArray(data, headers)
+
+  const [
+    {
+      topExtendStyle,
+      rightExtendStyle,
+      bottomExtendStyle,
+      leftExtendStyle,
+      extension,
+      topSelectStyle,
+      rightSelectStyle,
+      bottomSelectStyle,
+      leftSelectStyle,
+      colLineStyle,
+      rowLineStyle,
+      squareStyle
+    },
+    { watchCellSelection, computeCellExtendStyle }
+  ] = useSpreadSheet(table)
+</script>
+
+<div>
+  <!-- <CellValueEditor /> -->
+  <div class="relative" use:watchCellSelection>
+    <ExTable table={table} computeCellExtendStyle={computeCellExtendStyle} />
+    <ExtendOverlay
+      extension={$extension}
+      topStyle={$topExtendStyle}
+      rightStyle={$rightExtendStyle}
+      leftStyle={$leftExtendStyle}
+      bottomStyle={$bottomExtendStyle}
+    />
+    <SelectOverlay
+      topStyle={$topSelectStyle}
+      rightStyle={$rightSelectStyle}
+      leftStyle={$leftSelectStyle}
+      bottomStyle={$bottomSelectStyle}
+      colLineStyle={$colLineStyle}
+      rowLineStyle={$rowLineStyle}
+    />
+    <DraggableSquare squareStyle={$squareStyle} />
+  </div>
+</div>
---
+++ src/lib/excel/components/overlay/ExtendOverlay.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import type { TopStyle, RightStyle, LeftStyle, BottomStyle } from "@/lib/excel/hooks/use-sheet"
+  import { px } from "@/lib/excel/utils/css.ts"
+
+  export let extension: boolean
+
+  export let topStyle: TopStyle
+  export let rightStyle: RightStyle
+  export let leftStyle: LeftStyle
+  export let bottomStyle: BottomStyle
+</script>
+
+<div
+  class="top-extend absolute"
+  class:hidden={!extension}
+  style:width={px(topStyle.width)}
+  style:left={px(topStyle.left)}
+  style:top={px(topStyle.top)}
+/>
+<div
+  class="bottom-extend absolute"
+  class:hidden={!extension}
+  style:width={px(bottomStyle.width)}
+  style:left={px(bottomStyle.left)}
+  style:top={px(bottomStyle.top)}
+/>
+<div
+  class="left-extend absolute"
+  class:hidden={!extension}
+  style:height={px(leftStyle.height)}
+  style:top={px(leftStyle.top)}
+  style:left={px(leftStyle.left)}
+/>
+<div
+  class="right-extend absolute"
+  class:hidden={!extension}
+  style:height={px(rightStyle.height)}
+  style:top={px(rightStyle.top)}
+  style:left={px(rightStyle.left)}
+/>
+
+<style>
+  .absolute {
+    position: absolute;
+    z-index: 10;
+    transition: all 0.1s linear;
+  }
+
+  .top-extend,
+  .bottom-extend {
+    border-bottom: 2px solid #aaa;
+  }
+  .left-extend,
+  .right-extend {
+    border-left: 2px solid #aaa;
+  }
+
+  .hidden {
+    display: none;
+  }
+</style>
