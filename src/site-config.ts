6de272b7a44823d9e02ff1d2385c58ebf4af0dfa
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-02-22 13:59:46 +0900

    サイトのカテゴリ一覧を定数として定義

---
+++ src/site-config.ts
@@ -0,0 +1,21 @@
+export const SITE_TITLE = "Pocket Excel Learning"
+
+export const NAV = [
+  {
+    title: SITE_TITLE,
+    href: "/"
+  },
+  {
+    title: "問題集",
+    href: "/question"
+  },
+  {
+    title: "Excel関数リファレンス",
+    href: "/fn"
+  },
+  {
+    title: "プレイグラウンド",
+    href: "/sheet",
+    draft: true
+  }
+]
